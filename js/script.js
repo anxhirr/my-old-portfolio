8785ab70079fb62e99d7905376c5120826d0fbc4
anxhirr <anxhirr@gmail.com>
2022-05-24 13:54:26 +0200

    To-Do project 1st -v

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ const btnsOpenModal = document.querySelectorAll('.open-modal--btn');
 const btnsCloseModal = document.querySelectorAll('.btn--close-modal');
 const modalOverlay = document.querySelector('#modal--overlay');
 
+const imgs = document.querySelector('img');
+const socialIcons = document.querySelectorAll('.social-list__link');
+
 const showMenu = function () {
   menu.classList.add('active');
   menuBtn.classList.add('hide');
@@ -57,12 +60,14 @@ const closeModal = function (modal) {
 btnsOpenModal.forEach(openButton => {
   openButton.addEventListener('click', () => {
     openModal(modal);
+    imgs.style.visibility = 'hidden';
   });
 });
 
 btnsCloseModal.forEach(closeButton => {
   closeButton.addEventListener('click', () => {
     closeModal(modal);
+    imgs.style.visibility = 'unset';
   });
 });
 
@@ -70,5 +75,53 @@ modalOverlay.addEventListener('click', () => {
   const modals = document.querySelectorAll('.modal.active');
   modals.forEach(modal => {
     closeModal(modal);
+  });
+});
+
+socialIcons.forEach(sIcon => {
+  sIcon.addEventListener('click', () => {
+    console.log(socialIcons);
+    sIcon.style.opacity = '1';
+  });
+});
+
+// TO-DO PROJECT //
+
+const newTaskForm = document.querySelector('.new-task-form');
+const userInput = document.querySelector('#new-task-input');
+const tasks = document.querySelector('#tasks');
+
+newTaskForm.addEventListener('submit', e => {
+  e.preventDefault();
+  if (!userInput.value) {
+    alert('Please Add a Task Before Submiting');
+    return;
+  }
+
+  const taskHtml = `
+  <div class="tasks__task">
+    <div class="task__content">
+     <input
+       type="text"
+       class="task__content-text"
+       value="${userInput.value}"
+       readonly
+     />
+    </div>
+  <div class="task__actions">
+    <button class="btn btn-edit">Edit</button>
+    <button class="btn btn-delete">Delete</button>
+  </div>
+</div>`;
+
+  tasks.insertAdjacentHTML('afterbegin', taskHtml);
+
+  const btnEditTask = document.querySelector('.btn-edit');
+  const btnDeleteTask = document.querySelector('.btn-delete');
+
+  btnDeleteTask.addEventListener('click', () => {
+    console.log('sadfhjsdkal');
+    console.log(btnDeleteTask.closest('.tasks__task'));
+    btnDeleteTask.closest('.tasks__task').remove();
   });
 });
